20f10baf9fd27a385caea9c176580342b88217a7
Jesse012 <93549866+Jesse012@users.noreply.github.com>
2021-11-05 15:02:47 -0700

    update with tail stuff

--- Aircraft dimensions/DesignSizing.m
+++ Aircraft dimensions/DesignSizing.m
@@ -1,9 +1,13 @@
-WS = 6;
-AR = 14;
+WS = 17;
+AR = 18;
 MTOW = 414; %lbf
 
 lambda = linspace(0.6,1,10);
 TW = linspace(0.1733,1,10);
+Vh = 0.4;
+Vv = 0.04;
+% lh =
+lv = 6.65;
 
 for i = 1:length(TW)
     T(i) = MTOW * TW(i); %lbf
@@ -11,6 +15,10 @@ for i = 1:length(TW)
     b(i) = sqrt(S(i)*AR);
     Cr(i) = (2*S(i))/(b(i)*(1+lambda(i)));
     Ct(i) = lambda(i)*Cr(i);
+    c(i) = (Cr(i) + Ct(i))/2;
+%     Sh(i) = (S(i)*c(i)*Vh)/lh;
+    Sv(i) = (S(i)*b(i)*Vv)/lv;
+
 end
 
     
