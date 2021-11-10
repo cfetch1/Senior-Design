05e9899fcdfc84eb9a8aad4572d8aed6633e5d5c
arnoldgreg98 <gregarnold99@gmail.com>
2021-11-10 11:42:25 -0800

    Create runSizingALL.m
    parallel range and performance sizing outputs MTOW, engine, and planform parameters

---
+++ runSizingALL.m
@@ -0,0 +1,63 @@
+close all
+clear all
+clc
+
+Wpl = 60;
+Wtoguess = 400;
+Ecruise = 16;
+SFCcruise = .4;
+Range = 550;
+Eloiter = 20;
+SFCloiter = .4;
+Endurance = 1;
+reserve = 2*6;
+hcr = 7000;
+Vcl = 120;
+ROC = 40;
+SFCclimb = .5;
+eta_cl = .3132;
+eta_cr = .7641;
+Eclimb = 18;
+
+[MTOW,We,Wf]=RangeSizing(Wpl,Wtoguess,Ecruise,SFCcruise,Range,Eloiter,SFCloiter,Endurance,reserve,hcr,Vcl,ROC,SFCclimb,eta_cl,eta_cr,Eclimb);
+
+WS = 5:1:30;
+CD0 = .028;
+CDto = .038;
+CLto = .7;
+AR = 23;
+dg = 3000;
+
+Vcr = 120;
+Vturn = .5*Vcr;
+Vto = 40;
+V = [Vturn, Vcl, Vto, Vcr];
+
+eta = [.4354,eta_cl,.4309,eta_cr,.7667]; %[turn, climb, to, cruise, ceiling]
+
+hturn = hcr;
+hto = 0;
+hclimb = 0;
+hmax = 10000;
+h = [hturn, hclimb, hto, hcr, hmax];
+
+[PW,PWmin_,PWmin,ii] = PowerSizing(WS,CD0,CDto,CLto,AR,V,ROC,h,dg,eta,0);
+
+P = PWmin*MTOW; %shaft horsepower
+S = PWmin*MTOW; %planform area, ft^2
+b = sqrt(S*AR); %wingspan, ft
+c = S/b; %mean chord, ft
+
+
+
+
+
+
+
+
+
+
+
+
+
+
