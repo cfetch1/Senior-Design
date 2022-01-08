3bbc762e0b12da54bba75d5e7f27dc7a6abbf197
Heddie <42524625+130706472@users.noreply.github.com>
2022-01-07 18:46:02 -0800

    Add Cd_p sum equation

--- Wing Tail Drag Buildup/Xfoil699src/main_wing_parasitic.m
+++ Wing Tail Drag Buildup/Xfoil699src/main_wing_parasitic.m
@@ -1,8 +1,8 @@
 clc;clear;close all;
 
 % Trim Condition setup
-alpha = [9.8327	6.65967	4.62528	3.23895	2.25047...
-        1.52047	0.96596	0.53461	0.19257	-0.08331];
+alpha = [9.8327	6.65967	4.62528	3.22 2.25047...
+        1.52047	0.9655	0.53461	0.193 -0.08325];
 
 V = [101.28	118.16	135.04	151.92	168.8...
      185.68	202.56	219.44	236.32	253.2];
@@ -22,10 +22,38 @@ MAC = [2.00713 1.73938 1.47163 1.20388];
 % Area [ft2]
 S = [6.021 5.218 4.415 3.612];
 
+% Area without fuselage
+S_e = sum(S) - 1.5*2.141;
+
 % Re by section and speed
 for i = 1:length(V)
     Re(:,i) = (rho*V(i)*MAC)./mu;
 end
 
 
-% xfoil('NACA2412',9.8327,1089508,0.093)
+for i = 1:size(Re,2)
+    disp(['Speed = ', num2str(V(i)),'[ft/s]'])
+    top = 0;
+    bottom = 0;
+    
+    for j = 1:size(Re,1) 
+        % Run Xfoil
+        RSUT = xfoil('NACA2412', alpha(i), Re(j,i), M(i),'panels n 400');
+        disp(['Section = ', num2str(j),' Cd_p_i = ', num2str(RSUT.CDp)])        
+        
+        % Sum Equations
+        top = top + RSUT.CDp.*S(j);
+        bottom = bottom + S(j);
+    end
+
+    % Total C_d parasitic
+    Cd_p = 2*(top./bottom)*(S_e/sum(S));
+    disp(['Total Cd_p = ', num2str(Cd_p)])
+    disp(' ')
+end
+
+
+
+
+
+
