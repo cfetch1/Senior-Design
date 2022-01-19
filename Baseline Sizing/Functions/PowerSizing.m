812cfb82775ba412a8b9380ed3dc5c6399eac181
Heddie <42524625+130706472@users.noreply.github.com>
2022-01-19 00:11:33 -0800

    Constrain Diagram Script
    Rearranged constrain diagram script for initial sizing

--- Baseline Sizing/Functions/PowerSizing.m
+++ Baseline Sizing/Functions/PowerSizing.m
@@ -24,13 +24,14 @@ sigma = [rho(h(1))/rho_SL,rho(h(2))/rho_SL,rho(h(3))/rho_SL,rho(h(4))/rho_SL,rho
 [PW_dca_SL] = (TW_dca*V(4))/((sigma(4)^0.8)*eta(4));
 [PW_sc_SL] = (TW_sc*V(4))/((sigma(5)^0.8)*eta(5));
 
+
 PW_ = [PW_lcvt_SL; PW_dcr_SL; PW_dtod_SL; PW_dca_SL; PW_sc_SL]/550;
 
 
 
 for ii = 1:length(WS)
     PWmin_(ii) = max(PW_(:,ii));
-    WS_land(ii) = round(dg/(1.938*.25*(2*MTOW/(rho_SL*40*CLmax))));
+    WS_land(ii) = round(dg/(1.938*.25*2/(rho_SL*CLmax)));
     WS_stall(ii) = round(V(5)^2*rho_SL*CLmax/2); 
 end
 
@@ -49,61 +50,40 @@ if WS(index) > limWS
     if PWmin_(ii) == limWS
         index = ii;
     end
-    end
+  end
 end
 
-% [TW_lcvt_SL] = TW_lcvt/(sigma(1)^0.8);
-% [TW_dcr_SL] = TW_dcr/(sigma(2)^0.8);
-% [TW_dtod_SL] = TW_dtod/(sigma(3)^0.8);
-% [TW_dca_SL] = TW_dca/(sigma(4)^0.8);
-% [TW_sc_SL] = TW_sc/(sigma(5)^0.8);
-% 
-% TW_ = [TW_lcvt_SL; TW_dcr_SL; TW_dtod_SL; TW_dca_SL; TW_sc_SL];
-% TWmin = max(TW_(:,index))
-% 
+dy = linspace(0,max(PW_(4,:)),length(PW_(1,:)));
 
-dy = linspace(0,max(PW_(1,:)),length(PW_(1,:)));
-
+%% Plot constrain lines
 if plots == 1
     figure
     hold on
-    
-    
-    plot(WS,PW_(1,:),'r','linewidth',2)
-    plot(WS,PW_(2,:),'b','linewidth',2)
-    plot(WS,PW_(3,:),'g','linewidth',2)
-
-    plot(WS,PW_(4,:),'m','linewidth',2)
-    plot(WS,PW_(5,:),'c','linewidth',2)
+    plot(WS,PW_(4,:),'r','linewidth',2)
+    plot(WS,PW_(5,:),'b','linewidth',2)
     plot(WS_land,dy,'m','linewidth',2)
-    
-    hatchedline(WS,PW_(4,:),'r',pi/180,.5,1,1);
-    hatchedline(WS,PW_(5,:),'b',pi/180,.5,1,1);
-    hatchedline(WS_land,dy,'m',45*pi/180,.5,1,1);
-    
+    hatchedline(WS,PW_(4,:),'r',pi/180,.5, 0.75, 0.75);
+    hatchedline(WS,PW_(5,:),'b',pi/180,.5, 0.75, 0.75);    
 
-    for ii = 1:round(length(WS_land)/1.4)
-        plot([WS_land(1);WS_land(1)+1],[ii*1.4*.25/length(WS_land);ii*1.4*.25/length(WS_land)],'m')
-    end
+%   Add hatchline for vertical line
+    ii = 1;
+    while ii < 50
+        plot([WS_land(1); WS_land(1)+0.5],...
+            [ii*0.75*.25/length(WS_land); (ii-0.75)*0.75*.25/length(WS_land)],...
+            'm', 'LineWidth',2)
+        ii = ii + 1.5;
+    end 
     
-    
-%     plot(WS_stall,dy,'k--','linewidth',2)
     scatter(WS(index),PWmin,'k*','linewidth',5);
-    %text(WS(index)+.25,PWmin,['Minimum Required Power = ' num2str(round(PWmin,4)) ' hp per lbm'],'HorizontalAlignment','Left')
     text(WS(index)+.25,PWmin,'Design Point','HorizontalAlignment','Left') 
     grid on
-    ax=gca;
-    ax.XTick = 0:5:WS(end);
-    ax.XAxis.MinorTick='on';
-    ax.XAxis.MinorTickValues = 0:1:WS(end);
-    ax.YTick = 0:.01:1;
-    ax.YAxis.MinorTick='on';
-    ax.YAxis.MinorTickValues = 0:.001:1;
     ylabel('P/W [brake horsepower/lbm]')
     xlabel('W/S [lbm/ft^2]')
     legend('Level Constant Velocity Turn','Desired Climb Rate','Desired Takeoff Distance','Desired Cruise Airspeed','Service Ceiling','Landing Distance','Stall Speed','location','best')
     legend('Required Takeoff Distance','Required Cruise Airspeed','Required Landing Distance','location','best')
     axis([WS(1),WS(end),0,.25])
+    xlim([5 20])
+    ylim([0.04 max(PW_(4,:))])
 end
 
 
---
+++ Baseline Sizing/Initial_Sizing.m
@@ -0,0 +1,199 @@
+close all;clear;clc;
+% Add function floder to path
+addpath('Functions')
+
+%% -----------------------------Weight-----------------------------------
+
+% Max take off weight Initial guess [lbf]
+MTOW = 500;
+
+% Feul weight [Gal]
+Wf = 10;
+
+% Payload Weight [lb]
+Wpl = 50;
+
+% Fuel Reserved 1/4 Full
+reserve = Wf/4;
+
+%% -----------------------------Range-------------------------------------
+
+% Total Range [nm]
+Range = 550;
+
+% Loiter Time [hr]
+Endurance = 1;
+
+%% ----------------------------Coefficients-----------------------------
+
+% Initial AR guess - Scalar/Vector
+AR = 15;
+
+% Max Lift Coefficient
+CLmax = 2;
+
+% Take-off Drag Coeffiecnt
+CDto = .028;
+
+% Take-off Lift Coefficient
+CLto = .7;
+
+% 0 Cl Drag
+CD0 = .035;
+
+% Oswald efficiency factor
+e = 1.78*(1-0.045*AR^0.68)-0.64;
+
+% Second order coefficent
+k = 1/(pi*AR*e);
+    
+% Lift/Drag
+E = 1/2*sqrt(1/(CD0*k));
+    
+% Climb L/D
+Eclimb = 15;
+
+% Cruise L/D
+Ecruise = E;
+
+% L/D at Loiter
+Eloiter = E;
+
+%% -----------------------------Velocity---------------------------------
+
+% V Climb [knots]
+Vcl = 120;
+
+% Gound Run [ft]
+dg = 3000;
+
+% Cruise Velocity [kts]
+Vcr = 120;
+
+% Turn Velocity [kts]
+Vturn = .5*Vcr;
+
+% Take off velocity [kts]
+Vto = 60;
+
+% Stall velocity [kts]
+Vstall = 40;
+V = [Vturn, Vcl, Vto, Vcr, Vstall];
+
+% Rate of Climb [ft/min]
+ROC = 40;
+
+%% ----------------------------Propulsion--------------------------------
+
+% Fuel Consumption at Cruise
+SFCcruise = .4;
+
+% Fuel Consumption at Loiter
+SFCloiter = .4;
+
+% Climb Fuel Consumption [lb/(lb*hr)]
+SFCclimb = .5;
+
+% Porp Efficieny at Climb/Cruise
+eta_cl = .7865;
+eta_cr = .7859;
+
+% Engine Efficiency [turn, climb, to, cruise, ceiling]
+eta = [0.4147, eta_cl, 0.4097, eta_cr, 0.7859];
+
+%% ------------------Altitude at conditions [ft]--------------------------
+
+% Cruise Altitude [ft]
+hcr = 7000;
+hturn = hcr;
+hto = 0;
+hclimb = 0;
+hmax = 10000;
+h = [hturn, hclimb, hto, hcr, hmax];
+
+%% --------------------------Initialize-----------------------------------
+
+% Wing Loading [lb/ft2]
+WS = 5:50;
+
+% Initial Guess Power [Hp]
+P = 50;
+
+%% Convergence Iterations
+
+% Set Error for Iteration
+err=1000;
+while abs(err)> 0.5
+    % Store MTOW from previous iteration as e1
+    e1 = MTOW;
+    
+    % Take off Weight Guess
+    Wtoguess = MTOW*.9;
+    
+    % Fuel Reserved 1/4 Full
+    reserve = Wf/4;
+
+    % Input to Rang Sizing Function to update MTOW
+    [MTOW, We, Wf]=RangeSizing(Wpl,Wtoguess,Ecruise,SFCcruise,Range,...
+        Eloiter,SFCloiter,Endurance,reserve,hcr,Vcl,ROC,SFCclimb,eta_cl,...
+        eta_cr,Eclimb);
+    
+    % Store Updated MTOW as e2
+    e2 = MTOW;
+    
+    % Initial guess (from previous iteraiton) power
+    E1 = P;
+    
+    % Run Power Sizing Funcaiton
+    [~,~,PWmin,ii] = PowerSizing(WS,CD0,CDto,CLto,CLmax,AR,V,ROC,h,dg,eta,0);
+     
+    % Shaft horsepower
+    P = PWmin*MTOW;
+    
+    % planform area [ft^2]
+    S = MTOW/WS(ii);
+    
+    % Wingspan [ft]
+    b = sqrt(S*AR); 
+    
+    % mean chord [ft]
+    c = S/b;
+    
+    % Store Updated Power
+    E2 = P;
+    CLmax = WS(ii)/((40*1.69)^2*.0024/2);
+     
+    % Descend Angle (Temporary)
+    gamma = asin(40*1.69/120);
+    
+    % Cruise Dynamic Pressure
+    q = .5*.0024*(Vcr/1.69)^2;
+    CL = (MTOW*cos(gamma))/(q*S);
+    CD = CD0 + k*CL^2;
+    
+    % L/D
+    Eclimb = CL/CD;
+    
+    % Error for convergence
+    err = abs(100*(E1-E2)/E2)+abs(100*(e1-e2)/e2);
+end 
+
+% Generate Plot using PowerSizing Function
+[~,~,PWmin,ii] = PowerSizing(WS, CD0, CDto, CLto, CLmax, AR, V,...
+                             ROC, h, dg, eta, 1);
+
+%% Display Sizing Result
+disp('Sizing Result: ')
+disp(['P/W: ', num2str(PWmin), ' [bhp/lbm]'])
+disp(['W/S: ', num2str(WS(ii)), ' [lbm/ft]'])
+disp(['AR: ', num2str(AR), ' [/]'])
+disp(['MTOW: ', num2str(MTOW), ' [lbs]'])
+disp(['P_req: ', num2str(P), ' [hp]'])
+disp(['Area: ', num2str(S), ' [ft^2]'])
+disp(['Span: ', num2str(b), ' [ft]'])
+disp(['Chord: ', num2str(c), ' [ft]'])
+disp(' ')
+
+
+
+
