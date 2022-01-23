273382be985706e042b952b291ff5220c44ca9df
lexmar1999 <93549868+lexmar1999@users.noreply.github.com>
2022-01-22 21:27:14 -0800

    roskam corrections

--- Weight_estimate/Roskam_weight/roskam_emp.m
+++ Weight_estimate/Roskam_weight/roskam_emp.m
@@ -8,7 +8,7 @@ function [W_emp] = roskam_emp(MTOW, S_h, A_h, t_rh, S_v, A_v, t_rv, sw_a)
 % t_rv = vertical tail maximum root thickness in ft
 % sw_a = vertical tail quarter cord sweep angle
 
-w_v = (1.68*(MTOW^0.567)*(S_v^1.24)*(A_v^0.482)) / (15.6*(t_rv^0.747)*(cos(sw_a)^0.882));
-w_h = (3.184*(MTOW^0.887)*(S_h^0.101)*(A_h^0.138)) / (57.5*(t_rh^0.223)) ;
+w_v = (1.68*(MTOW^0.567)*(S_v^1.24)*(A_v^0.482)) / (639.95*(t_rv^0.747)*(cos(sw_a)^0.882));
+w_h = (3.184*(MTOW^0.887)*(S_h^0.101)*(A_h^0.138)) / (174.04*(t_rh^0.223)) ;
 W_emp = w_h + w_v ;
 end
