0aa89c7310e461ae46fe9c17f1cb7858d8077158
EddieHsieh <42524625+130706472@users.noreply.github.com>
2022-02-11 14:56:26 -0800

    Drag

--- Wing Tail Drag Buildup/Xfoil699src/main_wing_parasitic.m
+++ Wing Tail Drag Buildup/Xfoil699src/main_wing_parasitic.m
@@ -1,28 +1,32 @@
 clc;clear;close all;
 
 % Trim Condition setup
-alpha = [9.8327	6.65967	4.62528	3.22 2.25047...
-        1.52047	0.9655	0.53461	0.193 -0.08325];
+CL_trim = [1.0080	0.7401  0.5670	0.4480	0.3629	0.3	0.2520	0.2147	0.1851	0.1613	0.1417	0.1256	0.1120	0.1005	0.0907	0.0823	0.0750	0.0686	0.0630	0.0581	0.0537
+];
 
-V = [101.28	118.16	135.04	151.92	168.8...
-     185.68	202.56	219.44	236.32	253.2];
+V = [101.3	118.1	135.0	151.9	168.8	185.7	202.5	219.4	236.3	253.2	270.0	286.9	303.8	320.7	337.6	354.4	371.3	388.2	405.1	422.0	438.8
+];
 
 % mach calculation
-T = 493.7153; % R
+T = 454.4784; % R
 R = 1716.49;
-rho = 0.0019272;
+rho = 0.0013546;
 mu = 3.6E-7;
 M = V./sqrt(1.4*R*T);
 
 % Section Geometry for Re Calc
 
 % Mean Chord [ft] 
-MAC = [2.00713 1.73938 1.47163 1.20388];
+MAC = [5.747 4.981 4.2144 3.4482];
 
 % Ref Area [ft2]
-S = 2.*[6.021 5.218 4.415 3.612];
+S = 2.*[49.5414388 42.9382124 36.32981376 29.72486328];
 
-% Area without fuselage
+
+% Fuse Diameter [ft]
+F_d = 5;
+
+% Area without fuselage [ft^2]
 S_e = 2.*sum(S) - (1.5*2.141);
 
 % Re by section and speed
@@ -39,8 +43,8 @@ for i = 1:size(Re,2)
 
     for j = 1:size(Re,1) 
         % Run Xfoil
-        RSUT = xfoil('NACA2412', alpha(i), Re(j,i), M(i),'panels n 400');
-        disp([...'Section = ', num2str(j),' Cd_p_i = ', 
+        RSUT = xfoilCl('NACA2412', CL_trim(i), Re(j,i), M(i),'panels n 400');
+        disp(['Section = ', num2str(j),' Cd_p_i = ',... 
               num2str(RSUT.CD)])        
         
         % Sum Equations
@@ -49,10 +53,15 @@ for i = 1:size(Re,2)
     end
 
     % Total C_d parasitic
-    Cd_p(i) = 2*(top./bottom)*(S_e/sum(S.*2));
-    disp([...'Total Cd_p = ',
-         num2str(Cd_p)])
-    disp(' ')
+    try
+        Cd_p(i) = 2*(top./bottom)*(S_e/sum(S));
+        disp([...'Total Cd_p = ',
+             num2str(Cd_p(i))])
+        disp(' ')
+    catch
+        disp(' ')
+        continue
+    end
 end
 
 
