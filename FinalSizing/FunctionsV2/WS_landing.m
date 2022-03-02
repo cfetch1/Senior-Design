576da0ca75fcc84b089ef975048a90dcacc52bb7
kgriswold10 <74891689+kgriswold10@users.noreply.github.com>
2022-03-02 13:40:42 -0800

    Delete Old weight estimate functions and rename to old

--- FinalSizing/FunctionsV2/WS_landing.m
+++ FinalSizing/FunctionsV2/WS_landing.m
@@ -1,7 +1,14 @@
 function [WS] = WS_landing(h,dg,CLmax)
-% Roskam pg 125
+f=[0.0351   -0.0029    0.0210];
+CD = polyval(f,CLmax);
 rho = density(h);
-Vs = @(WS) sqrt(2*WS/(rho*CLmax));
-SL = @(WS) .5136*(Vs(WS)/1.69)^2;
-WS = fzero(@(WS) SL(WS)-dg,20);
+Vbr = @(WS) sqrt(2*WS/(rho*CLmax));
+mu = .5;
+LS =@(WS) 0.25 * rho * Vbr(WS)^2 * CLmax;
+
+DS = @(WS)0.25 * rho * Vbr(WS) ^ 2 * CD;
+
+WS = fzero( @(WS) dg - ( -Vbr(WS) ^ 2 * WS/ (2 * 32.174 * (DS(WS) - mu * (WS - LS(WS)))) ), 15 );
+
+
 end
