d6d4f6eeecfb9751039617ba51d2830a1e7e5a6d
Heddie <42524625+130706472@users.noreply.github.com>
2022-03-04 21:37:29 -0800

    Change Render Specs

--- InSizing/InSizing.m
+++ InSizing/InSizing.m
@@ -170,27 +170,27 @@ E3 = E2;
         figure
         hold on
         ii=1;
-        plot(WS,PW_cruise2(ii,:),'b','linewidth',2)
-        plot(WS,PW_to(ii,:),'r','linewidth',2)
+        plot(WS,PW_cruise2(ii,:),'b','linewidth',3)
+        plot(WS,PW_to(ii,:),'r','linewidth',3)
         dy = linspace(0,2*max([PW_cruise2(ii,:),PW_to(ii,:)]),100);
         dx = zeros(100,1);
         dx(:,1) = WS_;
         axis([0,max(WS),min([PW_cruise2(ii,:),PW_to(ii,:)]),max([PW_cruise2(ii,:),PW_to(ii,:)])])
-        plot(dx,dy,'k','linewidth',2)
+        plot(dx,dy,'k','linewidth',3)
 
 % Insert Hatchline
         hatchedline(WS,PW_cruise2(ii,:),'b',pi/180,.5,1,1);
         hatchedline(WS,PW_to(ii,:),'r',pi/180,.5,1,1);
    
         for ii = 1:3:round(length(dx)/1.4)
-            plot([dx(ii,1); dx(ii,1)+0.75],[dy(ii); dy(ii)-0.025],'k','LineWidth',2)
+            plot([dx(ii,1); dx(ii,1)+0.75],[dy(ii); dy(ii)-0.025],'k','LineWidth',3)
         end
         
         scatter(23.8, 0.0927,'filled','pentagram','SizeData',800)
 
         xlabel('Wing Loading [lb/ft^{2}]')
-        xlim([10 25])
-        ylim([0 0.3])
+        xlim([5 30])
+        ylim([0 0.31])
         ylabel('Power Loading [hp/lb]')
         grid on
         ax=gca;
@@ -202,7 +202,7 @@ E3 = E2;
         ax.YTick = 0:.1:30000;
         ax.YAxis.MinorTick='on';
         ax.YAxis.MinorTickValues = 0:.025:30000;
-        ax.FontSize=14;
+        ax.FontSize=20;
     legend(['V_{cruise} = ' num2str(Vcruise) ' kts'],'Takeoff Requirement','Landing Requirement','location','best')
 
 %          scatter(397/36,30/397,'mo','filled')
