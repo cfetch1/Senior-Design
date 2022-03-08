dec557daaa380eeabd6caa3026a9337f36334268
arnoldgreg98 <gregarnold99@gmail.com>
2022-03-08 11:37:58 -0800

    Update FlightEnvelope.m

--- Run Scripts/FlightEnvelope.m
+++ Run Scripts/FlightEnvelope.m
@@ -1,3 +1,4 @@
+close all
 clc; clear; %close all;
 
 % FlightEnvelope.m
@@ -13,11 +14,12 @@ clc; clear; %close all;
 
 % You will need this. Change it.
 % addpath('C:\Users\fetch\Documents\GitHub\Senior-Design\Functions')
-addpath('D:\Uncategorized Files\GitHub\Senior-Design\Functions');
+addpath('C:\Users\grega\Documents\GitHub\Senior-Design\Functions');
 
 % Altitude and velocity range
-h = 0:1000:18000;
-V = 10:5:180;
+h = 0:500:18000;
+V = 10:.5:200;
+V0 = 10;
 
 % Aircraft parameters
 W = 640;
@@ -26,20 +28,17 @@ FOS = 1;
 BHP0 = 60;
 ROC = 500;
 throttle = 1;
-Vc = 120;
+Vc = 140;
 CLmax = 1.3;
+f = [0.0351   -0.0029    0.0210];
 
 
 for jj = 1:length(h)
+
     % Calculate local density
     rho = density(h(jj));
 
     for ii = 1:length(V)
-        %     if V(ii)<=120
-        %         eta = .85*(V(ii)/120)^1.5;
-        %     else
-        %         eta = .85*(120/V(ii))^1.5;
-        %     end
 
         % Prop efficiency?
         eta = TR640(V(ii),Vc);
@@ -49,6 +48,10 @@ for jj = 1:length(h)
 
         % Get drag polar
         [CL,CD] = DragSLF(V(ii),W,h(jj),S,0);
+        
+        if CL>CLmax
+            index = ii;
+        end
 
         % Get required power in hp
         P_req(ii) = .5*rho*V_fps^3*S*CD/550;
@@ -60,15 +63,16 @@ for jj = 1:length(h)
         P_eng(ii) = throttle*BHP0*(sig-(1-sig)/7.55);
         
         % Get available engine power
-        P_avail(ii) = throttle*BHP0*(sig-(1-sig)/7.55)*eta;  
+        P_avail(ii) = P_eng(ii)*eta;  
 
-        % Get drag polar for climb
-        [CLc,CDc] = DragSLF2(V(ii),W,h(jj),S,ROC,FOS);
-
-        % Get power required for climb
-        Pc_req(ii) = (.5*rho*V_fps^3*S*CD + W*ROC/60)/(550);
- 
+%         % Get drag polar for climb
+%         [CLc,CDc] = DragSLF2(V(ii),W,h(jj),S,ROC,FOS);
+% 
+%         % Get power required for climb
+%         Pc_req(ii) = (.5*rho*V_fps^3*S*CD + W*ROC/60)/(550);
+%  
     end
+
 
 % dy = linspace(0,100,100);
 % vs = zeros(100,1);
@@ -87,6 +91,13 @@ for jj = 1:length(h)
 
 % Get intersections between P_req and P_avail to find velocity bounds
 [v_lim(jj,:)] = intersections(V,P_req,V,P_avail);
+
+if v_lim(jj,1)<V(index)    
+    v_lim(jj,1) = V(index);
+    clear index
+end
+
+clear P_req P_avail
 
 end
 
