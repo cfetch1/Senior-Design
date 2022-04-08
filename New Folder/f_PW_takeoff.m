de62c9621c1aa8cea82650731d6537f09bef8adc
arnoldgreg98 <gregarnold99@gmail.com>
2022-04-08 13:59:30 -0700

    Update f_PW_takeoff.m

--- New Folder/f_PW_takeoff.m
+++ New Folder/f_PW_takeoff.m
@@ -1,9 +1,10 @@
 function [PW] = f_PW_takeoff(WS,Sg,CL_max)
 rho = density(0);
+
 for jj = 1:length(WS)
     V_lof = 1.1*sqrt(WS(jj)/(.5*rho*CL_max));
     CL_to = WS(jj)/((.5*rho*(V_lof)^2));
-    PW(jj) = (Sg/(WS(jj)*CL_to/1.21)/550); % Roskam I pg 95
+    PW(jj) = WS(jj)/(Sg*CL_to/1.21); % Roskam I pg 95
 end
 end
 
